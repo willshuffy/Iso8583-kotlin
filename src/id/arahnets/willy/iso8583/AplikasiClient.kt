15f01b2bd6c60b34fdaa408362238370ae31ca68
willshuffy <saputrawilly26@gmail.com>
2021-01-19 16:46:01 +0700

    get request and response

--- src/id/arahnets/willy/iso8583/AplikasiClient.kt
+++ src/id/arahnets/willy/iso8583/AplikasiClient.kt
@@ -10,22 +10,45 @@ class AplikasiClient {
 
         val formatterBit7  = SimpleDateFormat("MMddHHmmss")
 
+        println("========================= REQUEST =========================")
+
         val   logonRequest :MutableMap<Int, String>  = LinkedHashMap()
         logonRequest[7] = formatterBit7.format(Date())
         logonRequest[11] = "834624"
         logonRequest[70] = "001"
 
+        //get bitmap request
         val client = AplikasiClient()
-        val bitmap = client.countBitmap(logonRequest)
-        println("Bitmap : $bitmap")
+        val bitmapRequest = client.countBitmap(logonRequest)
+        println("Bitmap : $bitmapRequest")
 
         //convert bitmap to binary
-        val strBitToBin = bitmap.toString(2)
-        println("Convert Binary : $strBitToBin")
+        val strBitToBinRequest = bitmapRequest.toString(2)
+        println("Convert Binary : $strBitToBinRequest")
 
         //convert bitmap to hexa
-        val strBitToHex = bitmap.toString(16)
-        println("Convert Hexa : $strBitToHex")
+        val strBitToHexRequest = bitmapRequest.toString(16)
+        println("Convert Hexa : $strBitToHexRequest")
+
+        println("========================= RESPONSE =========================")
+
+        val   logonResponse :MutableMap<Int, String>  = LinkedHashMap()
+        logonResponse[7] = formatterBit7.format(Date())
+        logonResponse[11] = "834624"
+        logonResponse[39] = "00"
+        logonResponse[70] = "001"
+
+        //get bitmap request
+        val bitmapResponse = client.countBitmap(logonResponse)
+        println("Bitmap : $bitmapRequest")
+
+        //convert bitmap to binary
+        val strBitToBinResponse = bitmapResponse.toString(2)
+        println("Convert Binary : $strBitToBinResponse")
+
+        //convert bitmap to hexa
+        val strBitToHexResponse = bitmapResponse.toString(16)
+        println("Convert Hexa : $strBitToHexResponse")
     }
 
     fun countBitmap(message: MutableMap <Int, String> ):BigInteger{
