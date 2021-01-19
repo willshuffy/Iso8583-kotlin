ff36f2c7d0660b38d8c4a6e30a95e6abf5911c14
willshuffy <saputrawilly26@gmail.com>
2021-01-19 15:18:58 +0700

    convert to hexa with hardcode

--- src/id/arahnets/willy/iso8583/AplikasiClient.kt
+++ src/id/arahnets/willy/iso8583/AplikasiClient.kt
@@ -15,13 +15,16 @@ class AplikasiClient {
         logonRequest[11] = "834624"
         logonRequest[70] = "001"
 
-        var bitmap: BigInteger = BigInteger.ZERO.setBit(128-0)
-        bitmap = bitmap.setBit(128-7+1)
-        bitmap = bitmap.setBit(128-11+1)
-        bitmap = bitmap.setBit(128-70+1)
+        var bitmap: BigInteger = BigInteger.ZERO.setBit(128-1)
+        bitmap = bitmap.setBit(128-7)
+        bitmap = bitmap.setBit(128-11)
+        bitmap = bitmap.setBit(128-70)
 
         val strBitToBin = bitmap.toString(2)
         println("Convert Binary : $strBitToBin")
+
+        val strBitToHex = bitmap.toString(16)
+        println("Convert Hexa : $strBitToHex")
     }
 
 }
